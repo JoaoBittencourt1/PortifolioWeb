90d105149b76a614f2c338f52a923c97bdc8e490
Joao <joaovictoralvesbittencourt@gmail.com>
2026-09-06 22:22:33 -0300

    ui: remove jb on top left

--- src/components/monogram/Monogram.jsx
+++
@@ -1,7 +0,0 @@
-import './Monogram.css';
-
-function Monogram({ className = '' }) {
-  return <span className={`monogram ${className}`}>JB</span>;
-}
-
-export default Monogram;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,5 @@
 import { motion as Motion } from 'framer-motion';
-import { Link, useLocation } from 'react-router-dom';
-import Monogram from '../monogram/Monogram.jsx';
+import { useLocation } from 'react-router-dom';
 import './Navbar.css';
 
 const NAV_LINKS = [
@@ -22,11 +21,6 @@ function Navbar() {
       transition={{ duration: 0.55, ease: [0.22, 1, 0.36, 1] }}
     >
       <div className="navbar-container">
-        <Link to="/#top" className="navbar-brand" aria-label="Voltar ao topo">
-          <Monogram />
-          <span className="navbar-name">João Bittencourt</span>
-        </Link>
-
         <nav className="nav-menu">
           {NAV_LINKS.map((item, i) => (
             <Motion.a
